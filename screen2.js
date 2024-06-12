94be5230af570ce1f84036b5eaab9433be6ae2c3
Analiese-exe <ao.ukeje@gmail.com>
2024-06-12 22:00:01 +0100

    Added responsiveness

--- screen2.js
+++ screen2.js
@@ -1,6 +1,18 @@
-const dropbtn = document.querySelector('.dropbtn');
-const dropdownContent = document.querySelector('.dropdown-content');
+const dropbtn = document.querySelector(".dropbtn");
+const dropdownContent = document.querySelector(".dropdown-content");
 
-dropbtn.addEventListener('click', function() {
-  dropdownContent.classList.toggle('show'); // Toggle show/hide class
+dropbtn.addEventListener("click", function () {
+  dropdownContent.classList.toggle("show"); // Toggle show/hide class
 });
+const imgElement = document.querySelector(".icon-image img");
+imgElement
+function switchImage() {
+ 
+
+  imgElement.style.opacity = 0;
+  setTimeout(() => {
+    imgElement.src = "/assets/Done.png";
+
+    imgElement.style.opacity = 1;
+  }, 300);
+}
